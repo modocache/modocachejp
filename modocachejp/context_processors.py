87c253daf96c51b81f248204e8f048d08f9615b1
modocache <modocache@gmail.com>
2012-03-25 11:50:38 +0900

    Made context processor nicer.

--- modocachejp/context_processors.py
+++ modocachejp/context_processors.py
@@ -1,7 +1,11 @@
 from django.contrib.sites.models import Site
 
 def site_details(request):
+    try:
+        current_site = Site.objects.get_current()
+    except Site.DoesNotExist:
+        current_site = None
     return {
-        'current_site': Site.objects.get_current(),
+        'current_site': current_site,
         'request': request,
     }
